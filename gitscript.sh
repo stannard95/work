f6b7a059bbc9cfd9ee27c7b51f999d231296ec8c
stannard95 <14066588@brookes.ac.uk>
2017-11-22 08:53:59 +0000

    new script

---
+++ gitscript.sh
@@ -0,0 +1,32 @@
+echo "completing commits..."
+
+
+cd Documents/classwork/w01d03/intro_to_git
+git status
+git add .
+git commit -m "commit 1"
+
+cd ~/work
+
+cd Documents/classwork/SpartaIdentity
+git status
+git add .
+git commit -m "commit 2"
+
+cd ~/work
+
+cd Documents/classwork/w07d02/dev-enviorments
+git status
+git add .
+git commit -m "commit 4"
+
+cd ~/work
+
+cd Documents/classwork/w08d03/cookbooks/app-server
+git status
+git add .
+git commit -m "commit 5"
+
+cd ~/work
+
+echo "Complete!"
