27f242b59b3051a316585f2aeab0d5f6aeae34fe
stannard95 <14066588@brookes.ac.uk>
2017-11-22 08:58:56 +0000

    stage 2 complete.

--- gitscript.sh
+++ gitscript.sh
@@ -1,32 +1,65 @@
 echo "completing commits..."
 
 
-cd Documents/classwork/w01d03/intro_to_git
+
+cd Documents/classwork/w04d02/sinatra
 git status
 git add .
 git commit -m "commit 1"
 
 cd ~/work
 
-cd Documents/classwork/SpartaIdentity
+cd Documents/classwork/w07d03/provisioning_lab/Archive
 git status
 git add .
 git commit -m "commit 2"
 
 cd ~/work
 
-cd Documents/classwork/w07d02/dev-enviorments
+cd Documents/classwork/w08d03/cookbooks/mongodb-server
 git status
 git add .
 git commit -m "commit 4"
 
 cd ~/work
 
-cd Documents/classwork/w08d03/cookbooks/app-server
+cd Documents/homework/blogger/blogger_app
 git status
 git add .
 git commit -m "commit 5"
 
 cd ~/work
+
+cd  Documents/homework/calculator-buttons
+git status
+git add .
+git commit -m "commit 6"
+
+cd ~/work
+
+cd Documents/homework/comment_views
+git status
+git add .
+git commit -m "commit7"
+
+cd ~/work
+
+cd Documents/homework/jack_and_the_cat/jack_cat
+git status
+git add .
+git commit -m "commit 8"
+
+cd ~/work
+
+cd Documents/homework/meme_tube
+git status
+git add .
+git commit -m "commit 9"
+
+cd ~/work
+
+git add .
+git commit -m "stage 2 complete."
+
 
 echo "Complete!"
