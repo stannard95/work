20ac847759c4cda9be4fbb9f08809c5d558ebf95
stannard95 <14066588@brookes.ac.uk>
2017-11-22 09:09:49 +0000

    added interal scripts directory

---
+++ internal_scripts/gitscript.sh
@@ -0,0 +1,23 @@
+echo "completing commits..."
+
+git st
+
+echo "stage? (y/n)"
+read choice
+
+if [ "$choice" = "y" ]
+then
+	git add .
+fi
+
+echo "commit? (y/n)"
+read choice
+
+if [ "$choice" = "y" ]
+then
+	echo "commit message?"
+	read message
+	git commit -m "$message"
+fi
+
+echo "Complete!"
